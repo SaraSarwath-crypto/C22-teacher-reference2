5329ac93bf08a14b73654e05f3658c6ac5536d78
SaraSarwath-crypto <80384166+SaraSarwath-crypto@users.noreply.github.com>
2021-07-19 13:34:59 +0530

    lime

--- sketch.js
+++ sketch.js
@@ -19,7 +19,7 @@ function setup(){
 }
 
 function draw(){
-    background("pink");
+    background("lime");
     Engine.update(engine);
     rectMode(CENTER);
    rect(object.position.x,object.position.y,40,40);
